bc7ec5fc82e7f3f450040a2f696c8999f7fb04a3
김현하 <khh@omnicns.com>
2018-02-22 12:02:01 +0900

    외부소스 사용 준비

---
+++ app/src/main/java/com/example/visualkhh/myapplication/view/pojo/SerializablePath.java
@@ -0,0 +1,20 @@
+package com.example.visualkhh.myapplication.view.pojo;
+
+import android.graphics.Path;
+
+import java.io.Serializable;
+
+/**
+ * Created by lht on 17/1/5.
+ */
+
+public class SerializablePath extends Path implements Serializable {
+
+    public SerializablePath() {
+        super();
+    }
+
+    SerializablePath(SerializablePath path) {
+        super(path);
+    }
+}
--- app/src/test/java/com/example/visualkhh/myapplication/KotlinTest.kt
+++ app/src/test/java/com/example/visualkhh/myapplication/KotlinTest.kt
@@ -8,10 +8,15 @@ import org.junit.Test
 class KotlinTest {
     @Test
     fun test(){
-       val f: Float = 127.039898F
-       val ff: Float = 37.661352F
-        println(f)
-        println(ff)
+//       val f: Float = 127.039898F
+//       val ff: Float = 37.661352F
+//        println(f)
+//        println(ff)
 
+        val p = Math.pow(2.0, 5.0)
+        println(p)
+        val s = Math.sqrt(7.0)
+        println(s)
+//        val distance = Math.sqrt(Math.pow((event.x - (startX + previousTranslateX)).toDouble(), 2.0) + Math.pow((event.y - (startY + previousTranslateY)).toDouble(), 2.0))
     }
 }
